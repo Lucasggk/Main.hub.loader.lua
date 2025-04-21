7378180b97f40a9afc52ddad21ce2a21fcddb71f
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-04-21 16:35:01 -0300

    Update Main.hub.loader.lua

--- Main.hub.loader.lua
+++ Main.hub.loader.lua
@@ -1,9 +1,9 @@
 local gameId = game.PlaceId
 
 if gameId == 537413528 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluent-ui/main/Gui.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-buildaboat/main/Loader.lua", true))()
 elseif gameId == 70876832253163 or gameId == 116495829188952 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus/main/Loader.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-DeadRails-/main/Loader.lua", true))()
 else
     print("jogo sem script ainda")
 end
