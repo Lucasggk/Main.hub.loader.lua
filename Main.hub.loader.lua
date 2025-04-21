b0fd42b17f257e7afc16441a87ad786ec47b45a7
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-04-21 16:06:22 -0300

    Update Main.hub.loader.lua

--- Main.hub.loader.lua
+++ Main.hub.loader.lua
@@ -3,7 +3,7 @@ local gameId = game.PlaceId
 if gameId == 537413528 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluent-ui/main/Gui.lua", true))()
 elseif gameId == 116495829188952 then
-    
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus/main/Loader.lua", true))()
 else
     print("jogo sem script ainda)
 end
