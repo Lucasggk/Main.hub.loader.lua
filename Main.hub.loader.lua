95614a4d29b4851eaf37659577f800841792a913
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-04-22 20:20:32 -0300

    Update Main.hub.loader.lua

--- Main.hub.loader.lua
+++ Main.hub.loader.lua
@@ -1,21 +1,15 @@
 local gameId = game.PlaceId
 
-script1 = "loadstring(game:HttpGet('https://raw.githubusercontent.com/Lucasggk/Fluentplus-buildaboat/main/Loader.lua', true))()"
-script2 = "loadstring(game:HttpGet('https://raw.githubusercontent.com/Lucasggk/Fluentplus-DeadRails-/main/Loader.lua', true))()"
-script3 = "loadstring(game:HttpGet('https://raw.githubusercontent.com/Lucasggk/Fluentplus-LegendsSpeed/main/Loader.lua', true))()"
-
-tab = {script1, script2, script3}
-
 if gameId == 537413528 then
-    loadstring(tab[1])()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-buildaboat/main/Loader.lua", true))()
 elseif gameId == 70876832253163 or gameId == 116495829188952 then
-    loadstring(tab[2])()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-DeadRails-/main/Loader.lua", true))()
 elseif gameId == 3101667897 then
-    loadstring(tab[3])()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-LegendsSpeed/main/Loader.lua", true))()
 elseif gameId == 16331984995 then
-    loadstring(tab[1])()
-    loadstring(tab[2])()
-    loadstring(tab[3])()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-buildaboat/main/Loader.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-DeadRails-/main/Loader.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-LegendsSpeed/main/Loader.lua", true))()
 else
     print("jogo sem script ainda")
 end
