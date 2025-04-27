4e7cb0911d6a4ed7f29bac25f91d61263d7cd839
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-04-27 17:54:48 -0300

    Update Main.hub.loader.lua

--- Main.hub.loader.lua
+++ Main.hub.loader.lua
@@ -6,8 +6,6 @@ elseif gameId == 70876832253163 or gameId == 116495829188952 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-DeadRails-/main/Loader.lua", true))()
 elseif gameId == 3101667897 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-LegendsSpeed/main/Loader.lua", true))()
-elseif gameId == 4442272183 or gameId == 7449423635 then 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-bloxfruits/main/Loader.lua", true))()
 elseif gameId == 16331984995 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-buildaboat/main/Loader.lua", true))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-DeadRails-/main/Loader.lua", true))()
