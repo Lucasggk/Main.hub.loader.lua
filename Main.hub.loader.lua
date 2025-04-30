33424f2cfaf60eb155208c495b86410106578db1
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-04-30 13:21:58 -0300

    Update Main.hub.loader.lua

--- Main.hub.loader.lua
+++ Main.hub.loader.lua
@@ -1,12 +1,20 @@
 local gameId = game.PlaceId
+IDs = [
+    537413528,
+    70876832253163,
+    116495829188952,
+    3101667897,
+    16331984995
+]
 
-if gameId == 537413528 then
+    
+if gameId == IDs then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-buildaboat/main/Loader.lua", true))()
-elseif gameId == 70876832253163 or gameId == 116495829188952 then
+elseif gameId == IDs then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-DeadRails-/main/Loader.lua", true))()
-elseif gameId == 3101667897 then
+elseif gameId == IDs then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-LegendsSpeed/main/Loader.lua", true))()
-elseif gameId == 16331984995 then
+elseif gameId == IDs then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-buildaboat/main/Loader.lua", true))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-DeadRails-/main/Loader.lua", true))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-LegendsSpeed/main/Loader.lua", true))()
