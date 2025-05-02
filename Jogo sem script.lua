62f3ba67ea5e626d866eee9807746b6d088701dd
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-01 23:45:51 -0300

    Update Jogo sem script.lua

--- Jogo sem script.lua
+++ Jogo sem script.lua
@@ -24,5 +24,5 @@ local a = Window:AddTab({
 
 a:AddParagraph({
     Title = "jogo sem suporte",
-    Description = string.format("Id do Jogo: ".. gameId)
+    Content = string.format("Id do Jogo: ".. gameId)
   })
