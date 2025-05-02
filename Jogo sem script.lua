d0ec93fd93bd86d3392f8e676d7e721bbae56428
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-01 23:32:10 -0300

    Update Jogo sem script.lua

--- Jogo sem script.lua
+++ Jogo sem script.lua
@@ -1,3 +1,28 @@
 gameId = game.PlaceId 
 
 
+local Fluent = loadstring(Game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/release.lua", true))() 
+
+
+
+
+local Window = Fluent:CreateWindow({
+    Title = "Jogo sem suporte",
+    SubTitle = "     Feito por Lucas Lucas",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(500, 350),
+    Acrylic = false,
+    Theme = "Dark",
+    Center = true,
+    IsDraggable = true
+})
+
+local a = Window:AddTab({
+    Title = "infos",
+    Icon = "home"
+  })
+
+a:AddParagraph({
+    Title = "jogo sem suporte",
+    Description = "Id atual: ".. Gameid,
+  })
