4d433e873b8fec08be8a728216e2ef3adf1a0e8f
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-02 01:24:12 -0300

    Create Teste.lua

---
+++ Teste.lua
@@ -0,0 +1,93 @@
+--// em breve //--
+
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
+
+-- Função global para verificação externa
+getgenv()._g = function()
+    return {
+        key = "" -- Deixe vazio ou preencha com a key válida externamente
+    }
+end
+
+-- Verificação direta se a key externa está correta
+local link_key = "key(isto tudo e uma key so copia e põe lá)"
+if _g().key == link_key then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/hub/main/Main.hub.loader.lua", true))()
+    return
+end
+
+-- Sistema normal (caso a key externa não esteja definida ou esteja incorreta)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/hub/main/GameId.lua", true))()
+
+if table.find(IDs, game.PlaceId) then
+local Fluent = loadstring(Game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/release.lua", true))() 
+
+local Window = Fluent:CreateWindow({
+    Title = "System key",
+    SubTitle = "sistema de key by Lucas",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(500, 350),
+    Acrylic = false,
+    Theme = "Dark",
+    Center = true,
+    IsDraggable = true
+})
+
+local key = Window:AddTab({
+    Title = "system key",
+    Icon = "rbxassetid://95214531579570"
+})
+
+local config = Window:AddTab({
+    Title = "config",
+    Icon = "settings"
+})
+
+keys = ""
+
+local Input = key:AddInput("KeyInput", {
+    Title = "Input your key",
+    Default = "",
+    Placeholder = "key",
+    Numeric = false,
+    Callback = function(Value)
+        keys = Value
+    end
+})
+
+key:AddButton({
+    Title = "check key",
+    Callback = function()
+        if keys == link_key then
+            task.wait(0.5)
+            Window:Destroy()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/hub/main/Main.hub.loader.lua", true))()
+        else
+            Fluent:Notify({
+                Title = "Erro",
+                Content = "Key incorreta!",
+                Duration = 3
+            })
+        end
+    end
+})
+
+key:AddButton({
+    Title = "get key",
+    Callback = function()
+        setclipboard("https://link-center.net/1345506/sistema-de-key-scripts")
+        Fluent:Notify({
+            Title = "Sucesso",
+            Content = "Link copiado!",
+            Duration = 3
+        })
+    end
+})
+
+key:AddParagraph({
+    Title = "A KEY SEMPRE SERÁ A MESMA!"
+})
+else
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/hub/main/Jogo%20sem%20script.lua", true))()
+end
