4423526a4b7abcb2acc742496f5a3ab67e52c137
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-01 23:52:48 -0300

    Update Jogo sem script.lua

--- Jogo sem script.lua
+++ Jogo sem script.lua
@@ -22,7 +22,31 @@ local a = Window:AddTab({
     Icon = "home"
   })
 
+local section = a:AddSection("jogo sem suporte/atual")
+
 a:AddParagraph({
     Title = "jogo sem suporte",
     Content = string.format("Id do Jogo: ".. gameId)
   })
+
+local section = a:AddSection("jogos com suporte: ")
+
+local MarketplaceService = game:GetService("MarketplaceService")
+
+for id, _ in pairs(Ids) do
+    local success, info = pcall(function()
+        return MarketplaceService:GetProductInfo(id, Enum.InfoType.Asset)
+    end)
+
+    if success and info and info.Name then
+        a:AddParagraph({
+            Title = string.format("Jogo com suporte: %s", info.Name),
+            Description = string.format("ID do jogo: %d", id)
+        })
+    else
+        a:AddParagraph({
+            Title = "Jogo com suporte: Nome não encontrado",
+            Description = string.format("ID do jogo: %d", id)
+        })
+    end
+end
