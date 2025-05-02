4d7a8123fc5133c732be769cba7edd34ce5ca9d3
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-01 21:02:55 -0300

    Update Main.hub.loader.lua

--- Main.hub.loader.lua
+++ Main.hub.loader.lua
@@ -14,7 +14,7 @@ if gameId == 537413528 then
 
     
 elseif gameId == 70876832253163 or gameId == 116495829188952 then 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-DeadRails-/main/Loader.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-DeadRails/main/Loader.lua", true))()
 
     
 elseif gameId == 3101667897 then
@@ -22,13 +22,13 @@ elseif gameId == 3101667897 then
 
 
 elseif table.find(seas, game.PlaceId) then
-    loadstring(Game:HttpGet("https://raw.githubusercontent.com/Lucasggk/BloxFruits-/main/Loader.lua", true))()
+    loadstring(Game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-bloxfruits/main/Loader.lua", true))()
     
 elseif gameId == 16331984995 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-buildaboat/main/Loader.lua", true))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-DeadRails-/main/Loader.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-DeadRails/main/Loader.lua", true))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-LegendsSpeed/main/Loader.lua", true))()
-    loadstring(Game:HttpGet("https://raw.githubusercontent.com/Lucasggk/BloxFruits-/main/Loader.lua", true))()
+    loadstring(Game:HttpGet("https://raw.githubusercontent.com/Lucasggk/Fluentplus-bloxfruits/main/Loader.lua", true))()
     
 else
     print("jogo sem script ainda")
