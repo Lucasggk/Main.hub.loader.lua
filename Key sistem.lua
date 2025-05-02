a35853dc47ff310ac6dfa581df3988b879199d62
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-02 01:33:42 -0300

    Update Key sistem.lua

--- Key sistem.lua
+++ Key sistem.lua
@@ -6,12 +6,7 @@ repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/hub/main/GameId.lua", true))()
 
 if table.find(IDs, game.PlaceId) then
-
 local Fluent = loadstring(Game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/release.lua", true))() 
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
-
-
 
 local Window = Fluent:CreateWindow({
     Title = "System key",
@@ -24,97 +19,61 @@ local Window = Fluent:CreateWindow({
     IsDraggable = true
 })
 
-SaveManager:SetLibrary(Fluent)
-InterfaceManager:SetLibrary(Fluent)
-SaveManager:IgnoreThemeSettings()
-
-    
-
 local key = Window:AddTab({
     Title = "system key",
     Icon = "rbxassetid://95214531579570"
-  })
+})
 
 local config = Window:AddTab({
     Title = "config",
     Icon = "settings"
-  })
+})
 
-InterfaceManager:BuildInterfaceSection(config)
-    
-keys = "" 
+keys = ""
+link_key = "key(isto tudo e uma key so copia e põe lá)"
 
-local Input = key:AddInput("", {
+local Input = key:AddInput("KeyInput", {
     Title = "Input your key",
     Default = "",
     Placeholder = "key",
-    Numeric = false, 
-    Finished = false,
+    Numeric = false,
     Callback = function(Value)
-        keys = tostring(Value)
+        keys = Value
     end
 })
-
-
-
-SaveManager:SetPath("KeySystem/" .. game.PlaceId)
-local savedKey = SaveManager:Load("LastValidKey")
-
-if savedKey then
-    Input:SetText(savedKey)
-    keys = savedKey
-    end
-    
-
-    
-link_key = tostring("key(isto tudo e uma key so copia e põe lá)")
-
-key:AddButton({ 
-  Title = "check key",
-  Callback = function()
-    if keys == link_key then
-      SaveManager:SetPath("KeySystem/" .. game.PlaceId)
-      SaveManager:Save("LastValidKey", keys)
-      task.wait(0.5)
-      Library:Destroy()
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/hub/main/Main.hub.loader.lua", true))()
-    else
-      print("key errado")
-    end
-end
-})
-    
-
 
 key:AddButton({
-  Title = "Get key",
-  Callback = function()
-   setclipboard("https://link-center.net/1345506/sistema-de-key-scripts")
-  end
- })
+    Title = "check key",
+    Callback = function()
+        if keys == link_key then
+            task.wait(0.5)
+            Window:Destroy()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/hub/main/Main.hub.loader.lua", true))()
+        else
+            Fluent:Notify({
+                Title = "Erro",
+                Content = "Key incorreta!",
+                Duration = 3
+            })
+        end
+    end
+})
 
+key:AddButton({
+    Title = "get key",
+    Callback = function()
+        setclipboard("https://link-center.net/1345506/sistema-de-key-scripts")
+        Fluent:Notify({
+            Title = "Sucesso",
+            Content = "Link copiado!",
+            Duration = 3
+        })
+    end
+})
 
-
-
-
-    
-
-
-    
 key:AddParagraph({
-  Title = "A KEY SEMPRE SERÁ A MESMA!"
- })
-
-
-
-
-
-
-
-
+    Title = "A KEY SEMPRE SERÁ A MESMA!"
+})
 else
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasggk/hub/main/Jogo%20sem%20script.lua", true))()
-
 end
-
